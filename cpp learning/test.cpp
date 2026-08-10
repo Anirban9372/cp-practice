1dfa87782b961af2b972ce7f46e899ef61a96328
Anirban <85406733+Anirban9372@users.noreply.github.com>
2026-08-10 10:56:54 +0530

    Test1

---
+++ cpp learning/test.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int a, b;
+    string x;
+    string s;
+    getline(cin, s);
+    cout << s << endl;
+    cin >> a >> b >> x;
+    cout << a << " " << b << " " << x << endl;
+    cout << "15" << endl;
+
+    return 0;
+}
