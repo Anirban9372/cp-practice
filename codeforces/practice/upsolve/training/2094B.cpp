78a774c5e3700c0b533a3d43003c56d55d696c3c
Anirban <85406733+Anirban9372@users.noreply.github.com>
2026-08-19 23:56:15 +0530

    Upsolving

---
+++ codeforces/practice/upsolve/training/2094B.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int n, m, l, r;
+    cin >> n >> m >> l >> r;
+    int l_prime = max(l, -m);
+    int r_prime = l_prime + m;
+    cout << l_prime << " " << r_prime << "\n";
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
