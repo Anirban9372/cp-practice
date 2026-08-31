44787e965e0f734ded64c143cd23d8cf3e984565
Anirban <85406733+Anirban9372@users.noreply.github.com>
2026-08-31 23:51:23 +0530

    Patterns

---
+++ striver/Basics/pattern11.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int start = 1;
+        if (i % 2 == 0)
+        {
+            start = 1;
+        }
+        else
+        {
+            start = 0;
+        }
+    }
+
+    return 0;
+}
