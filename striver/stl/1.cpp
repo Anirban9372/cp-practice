4dc9c098315a1fe9241c77281611d036eac6b79a
Anirban9372 <Anirban9372@users.noreply.github.com>
2026-09-19 02:41:29 +0530

    update

--- striver/stl/1.cpp
+++ striver/stl/1.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-// Utilities
+
 void explainPair()
 {
     pair<int, int> p = {1, 3};
-    // cout << p.first << " " << p.second;
     pair<int, pair<int, int>> q = {1, {3, 4}};
     cout << q.first << " " << q.second.first << " " << q.second.second << "\n";
     pair<int, int> arr[] = {{1, 2}, {3, 4}, {5, 6}};
-    cout << arr[1].second << "\n"; // array can be also stored as a pair type
+    cout << arr[1].second << "\n";
 }
+
 void explainTuple()
 {
     tuple<int, double, char> a = {2, 3.4, 'a'};
@@ -18,28 +18,87 @@ void explainTuple()
     cout << get<2>(a) << "\n";
 }
 
-// Containers
 void explainVector()
 {
     vector<int> v = {1, 2, 3};
     v.push_back(4);
     v.emplace_back(5);
+
     vector<pair<int, int>> vec;
     vec.push_back({1, 2});
-    vec.emplace_back(3, 4); // it automaticaaly thinks that the input is a pair so it puts on the curly braces by its own
-    vector<int>(10, 100);   // creates 10 value of 100 as an array elements.
-    vector<int>(10);        // stores 10 garbage value or 10 0's
+    vec.emplace_back(3, 4);
+
+    vector<int>(10, 100);
+    vector<int>(10);
+
     vector<int> v1(5, 20);
-    vector<int> v2(v1); // whole v1 gets copied into v2
-    // iterators
-    vector<int>::iterator it = v.begin(); // it stores the memory address
+    vector<int> v2(v1);
+
+    vector<int>::iterator it = v.begin();
     it++;
-    cout << *(it) << " "; // it'll print the value at adress of the second array element:2
+    cout << *(it) << " ";
+    cout << v.back() << "\n";
+
+    for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
+    {
+        cout << *(it) << " ";
+    }
+
+    for (auto it = v.begin(); it != v.end(); it++)
+    {
+        cout << *(it) << " ";
+    }
+
+    for (auto it : v)
+    {
+        cout << it;
+    }
+
+    v.erase(v.begin() + 1);
+    v.erase(v.begin() + 2, v.begin() + 4);
+
+    vector<int> v3 = {10, 12, 13};
+    v.insert(v.begin(), 20);
+    v.insert(v.begin() + 1, 2, 15);
+    v1 = {40, 41, 42};
+    v1.pop_back();
+    v2.swap(v1);
+    v.clear();
+    cout << v.empty() << "\n";
 }
+// list
+void explainList()
+{
+    list<int> l = {1, 2, 3};
+    l.push_back(4);
+    l.push_front(0);
+    l.pop_front();
+    l.reverse();
+    l.emplace_back(6);
+    l.emplace_front(10);
+    for (auto it : l)
+    {
+        cout << it << " ";
+    }
+    cout << "\n";
+}
+void explainDeque()
+{
+    deque<int> dq = {1, 2, 3, 4};
+    cout << dq.front() << "\n";
+    cout << dq.back() << "\n";
+    for (auto it : dq)
+    {
+        cout << it << " ";
+    }
+}
+
 int main()
 {
     explainPair();
     explainTuple();
     explainVector();
+    explainList();
+    explainDeque();
     return 0;
 }
